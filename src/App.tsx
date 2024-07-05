7cb4922f85d989636305afb30eacd3b0223ea666
Adam Hassan <sensiadm@gmail.com>
2024-07-05 11:24:21 +0100

    clean up app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import React, { useEffect, useState } from 'react';
 import './App.css';
-import { Actor, getActors } from './interfaces/Actor';
-import ActorCard from './components/ActorCard';
 import {FilmPage} from './pages/FilmPage';
 import { ActorPage } from './pages/ActorPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -10,33 +7,6 @@ import { ActorsPage } from './pages/ActorsPage';
 
 
 function App() {
-  const [cards, setCards] = useState<JSX.Element[]>([]);
-  const [Film,setFilm] = useState<JSX.Element>();
-
-
-  async function getCards(): Promise<JSX.Element[]> {
-    const data: Promise<Actor[]> = getActors(1);
-
-  
-    return (await data).map((actor: Actor) => (
-      <ActorCard key={actor.id} id={actor.id} firstName={actor.firstName} lastName={actor.lastName} />
-    ));
-  }
-
-
-  useEffect(() => {
-    async function fetchData() {
-      const cardsData = await getCards();
-      setCards(cardsData);
-    }
-    fetchData();
-    
-
-  }, []);
-
-
-
-
 
   return (
     <BrowserRouter>
